52007b130d3b6ce59246cb2494c1b3cf7c7636c1
Kumarmangalam Mishra <63307990+mangalam-123@users.noreply.github.com>
2022-09-09 21:06:32 +0530

    changed
    Done

--- c1-versions.tf
+++ c1-versions.tf
@@ -12,6 +12,8 @@ terraform{
       bucket = "my-terraform-bucket-sample"
       key = "terraform-tfstate-file/terraform.tfstate"
       region = "us-east-2"
+      access_key = "AKIAUJ26YJROIXGJZ5AJ"
+      secret_key = "p2clxV/UicX3mZPiF4U+GnYkwXSanLpe44V2tPYo"
       dynamodb_table = "terraform_table"
     }
 }
