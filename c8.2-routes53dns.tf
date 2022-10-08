3f3048fea31c9a701ad8eab4c106b20aab73bdf5
Kumarmangalam Mishra <63307990+mangalam-123@users.noreply.github.com>
2022-10-08 13:34:35 +0530

    Update c8.2-routes53dns.tf
    commited

--- c8.2-routes53dns.tf
+++ c8.2-routes53dns.tf
@@ -1,9 +1,9 @@
 
 
 resource "aws_route53_record" "myapp" {
-
+  
   zone_id = data.aws_route53_zone.mydomain.zone_id
-  name = "www.myappstore.tech"
+  name = "myapp.myappstore.tech"
   type = "A"
 
   alias {
